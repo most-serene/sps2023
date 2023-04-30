52df6d47e97a5578aac69a161461b5becb7dbf05
Davide Pizzolato <pizzolato.davide00@gmail.com>
2023-04-30 16:52:10 +0200

    Add person queries

--- moviesApp/app/queries.py
+++ moviesApp/app/queries.py
@@ -4,14 +4,28 @@ from . import models
 
 
 def get_movie_from_name(name: str):
-    movies = models.TitleBasics.objects.raw(f'SELECT * FROM title_basics WHERE LOWER("primaryTitle") LIKE %s', ['%' + name + '%'])
+    return models.TitleBasics.objects.raw(f'SELECT * FROM title_basics WHERE LOWER("primaryTitle") LIKE %s', ['%' + name + '%'])
     # movies = models.TitleBasics.objects.filter(primarytitle__icontains=name)
-    return movies
 
 
 def get_movie_from_pk(pk: str):
     try:
         return models.TitleBasics.objects.raw(f'SELECT * FROM title_basics WHERE tconst = %s', [pk])[0]
+    except:
+        raise KeyError()
+
+# endregion
+
+
+# region Movie
+
+def get_person_from_name(name: str):
+    return models.NameBasics.objects.raw(f'SELECT * FROM name_basics WHERE LOWER("primaryname") LIKE %s', ['%' + name + '%'])
+
+
+def get_person_from_pk(pk: str):
+    try:
+        return models.NameBasics.objects.raw(f'SELECT * FROM name_basics WHERE nconst = %s', [pk])[0]
     except:
         raise KeyError()
 
