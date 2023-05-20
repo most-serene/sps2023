1fd3d5955e0b9f8542c847353112a0d501ee7e7c
Alessio-Campa <878170@stud.unive.it>
2023-05-20 15:55:19 +0200

    Added query time calculation

--- moviesApp/app/views.py
+++ moviesApp/app/views.py
@@ -1,31 +1,34 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
-
-import random
-
+import time
 from . import models
 from . import queries
 
+
 def home(request):
+    file = open("./times.csv", "a")
     template = loader.get_template("app/index.html")
     movies = []
     subtitle = "Search a movie"
     if request.method == "GET" and "name" in request.GET.keys():
         subtitle = f'Results for query: "{request.GET["name"]}"'
+        start = time.time()
+        movies = queries.get_movie_from_name(request.GET["name"])
 
-        name = request.GET["name"]
-        if name[-1] == 'Â':
-            name = name[:-1]
-
-        movies = queries.get_movie_from_name(name)
-
-    return HttpResponse(template.render({"movies": movies, "subtitle": subtitle}, request))
+    response = HttpResponse(template.render({"movies": movies, "subtitle": subtitle}, request))
+    if request.method == "GET" and "name" in request.GET.keys():
+        end = time.time()
+        file.write(f"name,{end-start}\n")
+        file.close()
+    return response
 
 
 def movie_detail(request, tconst):
+    file = open("./times.csv", "a")
     template = loader.get_template("app/movieDetail.html")
     try:
+        start = time.time()
         movie = queries.get_movie_from_pk(tconst)
         rating = queries.get_rating_from_pk(tconst)
         principals = queries.get_principals_from_pk(tconst)
@@ -34,9 +37,16 @@ def movie_detail(request, tconst):
     except:
         print("Error")
         return redirect(home)
-    return HttpResponse(template.render({
+
+
+    response = HttpResponse(template.render({
         "movie": movie,
         "rating": rating,
         "actors": actor_names.items(),
         "cast": cast_names.items(),
     }, request))
+
+    end = time.time()
+    file.write(f"id,{end - start}\n")
+    file.close()
+    return response
